9c9a8b80befaef88940346f27935040b9c5f3144
kamyar arshi <37985246+kamyararshi@users.noreply.github.com>
2024-09-17 15:29:39 +0000

    windows .bat update

--- scripts/install_windows.bat
+++ scripts/install_windows.bat
@@ -6,15 +6,22 @@ REM call git --version >nul 2>&1 && ( echo git found ) || ( echo git not found. 
 
 call scripts/settings_windows.bat
 
-call conda create -y -n %CONDA_ENV_NAME% python=3.7
+call conda create -y -n %CONDA_ENV_NAME% python=3.9
 call conda activate %CONDA_ENV_NAME%
 
-call conda install -y numpy==1.19.0 scikit-image python-blosc==1.7.0 -c conda-forge
-call conda install -y pytorch==1.7.1 torchvision cudatoolkit=11.0 -c pytorch
+
 call conda install -y -c anaconda git
 
 REM ###FOMM###
 call rmdir fomm /s /q
 call git clone https://github.com/alievk/first-order-model.git fomm
+call git clone https://github.com/kamyararshi/LivePortrait.git
+call cd LivePortrait
+call pip install -r requirements.txt
+call pip install -U "huggingface_hub[cli]"
+call huggingface-cli download KwaiVGI/LivePortrait --local-dir pretrained_weights --exclude "*.git*" "README.md" "docs"
 
+call cd ..
 call pip install -r requirements.txt --use-feature=2020-resolver
+
+call echo "All installations done. Install torch and torchvision based on cuda version from nvcc -V according to https://pytorch.org/get-started/previous-versions/"
