a1ad7970ef9cfc32466e994390b3d73a5bd52397
unknown <37985246+kamyararshi@users.noreply.github.com>
2024-09-19 14:31:25 +0200

    windows run env name changed

--- run_windows.bat
+++ run_windows.bat
@@ -6,5 +6,5 @@ call conda activate %CONDA_ENV_NAME%
 
 set CONFIG=fomm/config/vox-adv-256.yaml
 
-set PYTHONPATH=%PYTHONPATH%;%CD%;%CD%/fomm
-call python afy/cam_fomm.py --config %CONFIG% --relative --adapt_scale --no-pad --checkpoint vox-adv-cpk.pth.tar %*
+set PYTHONPATH=%PYTHONPATH%;%CD%;%CD%/LivePortrait
+call python afy/cam_liveportrait.py --config %CONFIG% --relative --adapt_scale --no-pad --checkpoint vox-adv-cpk.pth.tar %*
